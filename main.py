20aaff89adee004e2918c2d05759aabb2020f580
luna <w.kewk@yandex.ru>
2025-08-04 14:31:38 +0300

    authorization with jwt tokens

--- main.py
+++ main.py
@@ -1,32 +1,81 @@
 from fastapi import FastAPI, HTTPException, status, Depends
 from fastapi.responses import RedirectResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from models import DBURL, User
+from schemas import URL, URLBase, UserCreate, UserResponse, Token, TokenData
+
 from database import get_db, Base, engine
-from datetime import datetime, timezone
-from schemas import URL, URLBase, UserCreate, UserResponse
+from datetime import datetime, timezone, timedelta
+
 from passlib.context import CryptContext
+from jose import JWTError, jwt
 import secrets
 
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+pwd_context = CryptContext(schemes=['sha256_crypt'], deprecated='auto')
+
+SECRET_KEY = secrets.token_urlsafe(32)
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 1440
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
 app = FastAPI()
 
+def create_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    
+    if not expires_delta:
+        time = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    else: 
+        time = datetime.now(timezone.utc) + expires_delta
+    
+    data['exp'] = time
+    token = jwt.encode(claims=data, key=SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)) -> User:
+    
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    try:
+        payload = jwt.decode(token, key=SECRET_KEY, algorithms=ALGORITHM)
+        username: str = payload.get('sub')
+        if username is None:
+            raise credentials_exception
+        
+        tokendata = TokenData(username=username)
+        
+    except JWTError:
+        raise credentials_exception
+    
+    user = await db.execute(select(User).filter_by(username=username, is_active=True))
+    user = user.scalar_one_or_none()
+    if not user:
+        raise credentials_exception
+    return user
+    
 def hash_password(password: str) -> str:
-    hashed = pwd_context.hash(password)
-    return hashed
+    hashed_password = pwd_context.hash(password)
+    return hashed_password
 
 def check_password(plain_password: str, hashed_password: str) -> bool:
-    checked = pwd_context.check(plain_password, hashed_password)
+    checked = pwd_context.verify(plain_password, hashed_password)
     return checked
 
 @app.on_event("startup")
-async def startup():
+async def startup() -> None:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
 @app.post('/short', response_model=URL, status_code=status.HTTP_201_CREATED)
-async def short(url: URLBase, db: AsyncSession = Depends(get_db)):
+async def short(url: URLBase, db: AsyncSession = Depends(get_db)) -> DBURL:
     short_code = secrets.token_urlsafe(6)
     db_url = DBURL(
         long_url=str(url.long_url),
@@ -72,8 +121,26 @@ async def register_user(user_data: UserCreate, db: AsyncSession = Depends(get_db
     await db.refresh(user)
     
     return user
+
+@app.post('/token', response_model=Token)
+async def get_token(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)) -> Token:
+    
+    user_result = await db.execute(select(User).filter_by(username=form_data.username, is_active=True))
+    user = user_result.scalar_one_or_none()
+    
+    if not user or not check_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Неверное имя пользователя или пароль')
+    
+    new_token = create_token(data={'sub': user.username})
+    token = Token(access_token=new_token)
+    return token
+
+@app.get('/users/me', response_model=UserResponse)
+async def get_user(current_user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)) -> User:
+    return current_user
+    
+
     
 @app.get('/')
 async def start():
     return 'Lunas api'
-
--- schemas.py
+++ schemas.py
@@ -24,3 +24,9 @@ class UserResponse(BaseModel):
     class Config:
         from_attributes = True
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str = 'bearer'
+
+class TokenData(BaseModel):
+    username: str | None
